fcf5e09405317db8c50c464dba67bdc691e1de45
Andy Portmen <andy.portmen@gmail.com>
2020-01-28 10:53:27 +0000

    travis 8

--- update.sh
+++ update.sh
@@ -20,8 +20,8 @@ upload() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://andy-portmen:${GH_TOKEN}@github.com/andy-portmen/country-flags.git > /dev/null 2>&1
-  git push origin master --quiet
+  git remote add origin https://andy-portmen:${GH_TOKEN}@github.com/andy-portmen/country-flags.git
+  git push origin master
 }
 
 setup
