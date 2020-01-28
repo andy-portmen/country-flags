8e58488d9e80e5843200dfc2309e687e1a9abbb2
Andy Portmen <andy.portmen@gmail.com>
2020-01-28 09:03:40 +0000

    travis 4

--- update.js
+++ update.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+fs.readdir('/home/travis/build/andy-portmen/country-flags/node_modules/geolite2-redist/dbs', function(err, items) {
+  console.log(err, items);
+});
+
+
 const geolite2 = require('geolite2-redist');
 geolite2.open('GeoLite2-Country', path => {
   console.log(path);
 });
 console.log(111);
+
+
