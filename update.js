a5ffd88826be39740f681a81dab2d92db9de4b24
Andy Portmen <andy.portmen@gmail.com>
2020-01-28 09:10:04 +0000

    travis 5

--- update.js
+++ update.js
@@ -1,3 +1,4 @@
+console.log(12);
 const fs = require('fs');
 fs.readdir('/home/travis/build/andy-portmen/country-flags/node_modules/geolite2-redist/dbs', function(err, items) {
   console.log(err, items);
@@ -5,7 +6,7 @@ fs.readdir('/home/travis/build/andy-portmen/country-flags/node_modules/geolite2-
 
 
 const geolite2 = require('geolite2-redist');
-geolite2.open('GeoLite2-Country', path => {
+geolite2.open('GeoLite2-City', path => {
   console.log(path);
 });
 console.log(111);
