045a1c7ba6b58accb6f96229cf5191f3aa983d5a
Andy Portmen <andy.portmen@gmail.com>
2020-01-28 11:25:09 +0000

    travis 13

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-git add .
+git checkout master
+git add *.db
 git commit -am "Travis update: `date "+%b %Y"` (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]" --author="Travis CI <builds@travis-ci.org>"
 git push --force https://andy-portmen:${GH_TOKEN}@github.com/andy-portmen/country-flags.git master
 
