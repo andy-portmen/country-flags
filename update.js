3471f7bdc9ae620e435d41e4facad407c29c3875
Andy Portmen <andy.portmen@gmail.com>
2020-02-26 12:10:57 +0000

    .

--- update.js
+++ update.js
@@ -5,8 +5,8 @@ geolite2.open('GeoLite2-Country', path => {
   fs.stat(path, (err, s) => {
     console.log('File Size', s.size);
     if (s.size > 2 * 1024 * 1024) {
-      console.log('Updating the DB');
       fs.createReadStream(path).pipe(fs.createWriteStream('country-flags/firefox/data/assets/GeoLite2-Country.db'));
+      console.log('Updating GeoLite2-Country.db');
     }
   });
   return {};
