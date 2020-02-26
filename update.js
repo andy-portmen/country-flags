73ea7e2776f007b2b4dea3fe6c99340ba0f443f0
Andy Portmen <andy.portmen@gmail.com>
2020-02-26 12:06:28 +0000

    do not update a corrupted database

--- update.js
+++ update.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 const geolite2 = require('geolite2-redist');
 
 geolite2.open('GeoLite2-Country', path => {
-  fs.createReadStream(path).pipe(fs.createWriteStream('country-flags/firefox/data/assets/GeoLite2-Country.db'));
-
+  fs.stat(path, (err, s) => {
+    console.log('File Size', s.size);
+    if (s.size > 2 * 1024 * 1024) {
+      console.log('Updating the DB');
+      fs.createReadStream(path).pipe(fs.createWriteStream('country-flags/firefox/data/assets/GeoLite2-Country.db'));
+    }
+  });
   return {};
 });
