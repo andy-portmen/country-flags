67a93be9d1789311b1c0b3bb77e13d6233836e95
Andy Portmen <andy.portmen@gmail.com>
2020-07-07 15:03:40 +0200

    run git after updating db

--- update.js
+++ update.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 const geolite2 = require('geolite2-redist');
+const { exec } = require('child_process');
 
 geolite2.open('GeoLite2-Country', path => {
   fs.stat(path, (err, s) => {
     console.log('File Size', s.size);
     if (s.size > 2 * 1024 * 1024) {
-      fs.createReadStream(path).pipe(fs.createWriteStream('country-flags/firefox/data/assets/GeoLite2-Country.db'));
+      const stream = fs.createReadStream(path);
+      stream.on('end', () => exec('./update.sh', (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+        }
+        else {
+          console.log('stdout', stdout);
+          console.log('stderr', stderr);
+        }
+      }));
       console.log('Updating GeoLite2-Country.db');
+      stream.pipe(fs.createWriteStream('country-flags/firefox/data/assets/GeoLite2-Country.db'));
     }
   });
   return {};
