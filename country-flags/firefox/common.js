6b71bd8a62e74486ecdc92ac79b69f608dbc76d5
Andy Portmen <andy.portmen@gmail.com>
2021-06-05 01:55:28 -0400

    .

--- country-flags/firefox/common.js
+++ country-flags/firefox/common.js
@@ -558,10 +558,11 @@ chrome.storage.local.get(prefs, ps => {
         if (reason === 'install' || (prefs.faqs && reason === 'update')) {
           const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
           if (doUpdate && previousVersion !== version) {
-            tabs.create({
+            tabs.query({active: true, currentWindow: true}, tbs => tabs.create({
               url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
-              active: reason === 'install'
-            });
+              active: reason === 'install',
+              ...(tbs && tbs.length && {index: tbs[0].index + 1})
+            }));
             storage.local.set({'last-update': Date.now()});
           }
         }
