a32cf8c3f31b6e6cc0a0b552b9b8a49602cd1d1b
Andy Portmen <andy.portmen@gmail.com>
2023-04-08 00:50:00 -0400

    version 0.6.1

--- v3/country-flags/existing.js
+++ v3/country-flags/existing.js
@@ -1,29 +1,32 @@
-/* globals onResponseStarted, xDNS, update, pp */
+/* global onResponseStarted, xDNS, update, pp */
 'use strict';
 
 {
-  const once = () => chrome.tabs.query({
-    url: '*://*/*'
-  }, tbs => {
-    for (const tab of tbs) {
-      if (tab.url && tab.url.startsWith('http')) {
-        xDNS(tab.url).then(d => onResponseStarted({
-          reason: 'xDNS:existing:resolved',
-          ip: d.ip,
-          tabId: tab.id,
-          url: d.url,
-          type: 'main_frame',
-          timeStamp: Date.now()
-        })).catch(async e => {
-          await pp.set(tab.id, {
-            error: e.message
+  const once = () => {
+    clearTimeout(once.id);
+    once.id = setTimeout(() => chrome.tabs.query({
+      url: '*://*/*'
+    }, tbs => {
+      for (const tab of tbs) {
+        if (tab.url && tab.url.startsWith('http')) {
+          xDNS(tab.url).then(d => onResponseStarted({
+            reason: 'xDNS:existing:resolved',
+            ip: d.ip,
+            tabId: tab.id,
+            url: d.url,
+            type: 'main_frame',
+            timeStamp: Date.now()
+          })).catch(async e => {
+            await pp.set(tab.id, {
+              error: e.message
+            });
+            update(tab.id, 'xDNS:existing:rejected', tab);
+            console.warn('Cannot resolve using xDNS', tab.url, e.message);
           });
-          update(tab.id, 'xDNS:existing:rejected', tab);
-          console.warn('Cannot resolve using xDNS', tab.url, e.message);
-        });
+        }
       }
-    }
-  });
+    }), 100);
+  };
 
   chrome.runtime.onInstalled.addListener(once);
   chrome.runtime.onStartup.addListener(once);
